5814ab53ff309562a1dbb7749e8d176cbba987a3
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-01-11 16:22:45 +0600

    nodejs global object and module system introduction

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// setTimeout(() => {
+//     console.log('Test');
+// }, 1000);
+// var a = 10;
+// console.log(global.a);
+
+const people = require('./test/people');
 const _ = require('lodash');
 
-console.log(_.last([1, 2, 3]));
+console.log(people);
+
+console.log(_.last(people));
---
+++ test/people.js
@@ -0,0 +1,13 @@
+// (function (exports, require, module, __filename, __dirname) {
+  var people = ["tahsin", "ferdous", "cubox"];
+
+  var a = 10;
+
+  function multiply(x, y) {
+    return x * y;
+  }
+
+  module.exports = people;
+
+  // return module.exports;
+// });
